8528055d8c761ecdd2739a33713197aabb59bba3
Guillaume Savaton <guillaume.savaton@eseo.fr>
2022-02-11 14:56:11 +0100

    Dithered background image

--- .config/awesome/theme/theme.lua
+++ .config/awesome/theme/theme.lua
@@ -99,7 +99,7 @@ theme.titlebar_maximized_button_focus_inactive  = theme_path .. "/titlebar/maxim
 theme.titlebar_maximized_button_normal_active   = theme_path .. "/titlebar/maximized_normal_active.png"
 theme.titlebar_maximized_button_focus_active    = theme_path .. "/titlebar/maximized_focus_active.png"
 
-theme.wallpaper = theme_path .. "/background.jpg"
+theme.wallpaper = theme_path .. "/background.png"
 
 -- You can use your own layout icons like this:
 theme.layout_fairh      = theme_path .. "/layouts/fairhw.png"
